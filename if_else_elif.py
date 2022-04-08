d17f302fe9c232b2467a6baa2e2fad2b78a6b677
Rafael Corretori <rcorretori@neotel.com.br>
2022-04-08 12:34:42 -0300

    Começo estudos python 6 - Básico de condições if, else e elif

---
+++ if_else_elif.py
@@ -0,0 +1,16 @@
+"""
+Estruturas condicionais
+if(se), else(se não), elif(else if)
+
+"""
+
+idade = 20
+
+if idade < 18:
+    print ('Menor de idade')
+elif idade == 18:
+    print('Tem 18 anos')
+elif idade == 26:
+    print('Tem 26 anos')
+else:
+    print ('Maior de idade')
