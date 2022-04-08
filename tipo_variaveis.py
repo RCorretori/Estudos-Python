55db8aeb808f2a7205e91e4bfb4a65c982f0f7f8
Rafael Corretori <rcorretori@neotel.com.br>
2022-04-08 11:59:58 -0300

    Começo estudos python 5 - Básico de Variaveis

---
+++ tipo_variaveis.py
@@ -0,0 +1,22 @@
+"""
+Para declarar uma variavel:
+
+variavel(qualquer nome que você definir) = valor da variavel(tanto nome quanto numero, você define)
+
+"""
+
+numero = 42
+print(numero)
+print(type(numero))
+
+numero = 'Geek'
+print(numero)
+print(type(numero))
+
+nao_existo = 'Oi'
+print(nao_existo)
+
+numero = 42
+if numero > 10:
+    novo = numero + 10
+    print(novo)
